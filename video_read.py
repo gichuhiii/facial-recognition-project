4c0edba5f84a69451010406c3a94e2065638fd30
gichuhiii <tashakuigichuhi@gmail.com>
2023-12-30 21:20:01 +0300

    Trey was here

---
+++ video_read.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+" The following code checks if webcam is running "
+
+import cv2
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    ret, frame = cap.read()
+
+    if ret == False:
+        continue
+
+
+    cv2.imshow("video frame", frame)
+
+    key_pressed = cv2.waitKey(1) & 0xFF
+
+    if key_pressed == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
